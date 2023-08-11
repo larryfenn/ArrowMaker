ce4af1920bfbc64c3e3205631c3402c77e3bcfc9
Larry Fenn <larry.fenn@gmail.com>
2023-08-10 22:18:28 -0400

    Add configurable device transmission delay

--- ArrowMaker/Program.cs
+++ ArrowMaker/Program.cs
@@ -12,8 +12,10 @@ namespace ArrowMaker
             Dictionary<int, Device> devices = new Dictionary<int, Device>();
             int port = 5005;
             int n_devices = 1;
+            int delay = 50;
             // first arg is # of devices
             // second arg if it exists is port
+            // third arg is time between device transmissions
             if (args.Length > 0)
             {
                 n_devices = Int32.Parse(args[0]);
@@ -22,6 +24,11 @@ namespace ArrowMaker
             if (args.Length > 1)
             {
                 port = Int32.Parse(args[1]);
+            }
+
+            if (args.Length > 2)
+            {
+                delay = Int32.Parse(args[2]);
             }
 
             for (int i = 0; i < n_devices; i++)
@@ -35,11 +42,13 @@ namespace ArrowMaker
 
             while(true)
             {
+                Random rand = new Random();
                 foreach(KeyValuePair<int, Device> entry in devices)
                 {
                     entry.Value.Update();
                     entry.Value.Transmit(endpoint, socket);
                 }
+                System.Threading.Thread.Sleep(delay);
             }
         }
     }
